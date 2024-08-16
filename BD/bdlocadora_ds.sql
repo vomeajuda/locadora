32560786dcc567db3afa428c428fdbc7fd637a74
vomeajuda <leogugo9@hotmail.com>
2024-08-16 10:48:31 -0300

    boom boom boom

--- BD/bdlocadora_ds.sql
+++ BD/bdlocadora_ds.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09-Ago-2024 às 16:30
+-- Tempo de geração: 16-Ago-2024 às 15:46
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -20,6 +20,17 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `bdlocadora_ds`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `CatCod` int(11) NOT NULL,
+  `CatNome` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -41,6 +52,17 @@ CREATE TABLE `clientes` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `DeptoCod` int(11) NOT NULL,
+  `DeptoNome` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `funcionarios`
 --
 
@@ -55,9 +77,32 @@ CREATE TABLE `funcionarios` (
   `funcAtivo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `veiculos`
+--
+
+CREATE TABLE `veiculos` (
+  `veicPlaca` varchar(7) NOT NULL,
+  `veicMarca` varchar(15) NOT NULL,
+  `veicModelo` varchar(15) NOT NULL,
+  `veicCor` varchar(15) DEFAULT NULL,
+  `veicAno` int(4) NOT NULL,
+  `veicComb` varchar(1) DEFAULT NULL,
+  `veicCat` int(1) DEFAULT NULL,
+  `veicStatusAlocado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`CatCod`);
 
 --
 -- Índices para tabela `clientes`
@@ -66,10 +111,22 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`clienteCPF`);
 
 --
+-- Índices para tabela `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`DeptoCod`);
+
+--
 -- Índices para tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
   ADD PRIMARY KEY (`funcMatricula`);
+
+--
+-- Índices para tabela `veiculos`
+--
+ALTER TABLE `veiculos`
+  ADD PRIMARY KEY (`veicPlaca`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
