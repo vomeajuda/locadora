bcd75daf6270819a6abf765d05b7f1ef69c77e19
vomeajuda <leogugo9@hotmail.com>
2024-11-06 20:53:36 -0300

    já pode ir pra casa tio?

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
+const mysql = require("mysql");
+const myConnection = require("express-myconnection");
 const path = require('path');
 const app = express();
+require('dotenv').config();
+
+const PASS = process.env.DATABASE_PASSWORD;
+
+const rotas = require('./routers/cliente');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'site'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: '3306',
+    database: 'bdlocadora_ds'
+}, 'single'));
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', rotas);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
     console.log('Server on port 3000');
